bce379976114a6ce5d9281eedcdd5fa5ec6b260c
Kapalee <Shamvictor@hotmail.com>
2024-12-29 19:22:35 +0100

    Created icons for tabbar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,8 @@
-import { Tabs } from 'expo-router';
-import { useColorScheme } from 'react-native';
+import { Tabs } from "expo-router";
+import { useColorScheme } from "react-native";
+import Entypo from "@expo/vector-icons/Entypo";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -9,36 +12,54 @@ export default function TabLayout() {
       screenOptions={{
         headerShown: false,
         tabBarStyle: {
-          backgroundColor: colorScheme === 'dark' ? '#1f2937' : 'white',
+          backgroundColor: colorScheme === "dark" ? "#1f2937" : "white",
         },
-        tabBarActiveTintColor: colorScheme === 'dark' ? '#fff' : '#000',
-      }}>
+        tabBarActiveTintColor: colorScheme === "dark" ? "#fff" : "#000",
+      }}
+    >
       <Tabs.Screen
         name="SelectRecipes"
         options={{
-          title: 'Planlæg',
-          tabBarLabel: 'Planlæg',
+          title: "Planlæg",
+          tabBarLabel: "Planlæg",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="list-ul" color={color} size={size} />
+          ),
         }}
       />
+
       <Tabs.Screen
-        name="Recipe"
+        name="Recipes"
         options={{
-          title: 'Opskrifter',
-          tabBarLabel: 'Opskrifter',
+          title: "Opskrifter",
+          tabBarLabel: "Opskrifter",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="food-turkey"
+              color={color}
+              size={size}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="ShoppingList"
         options={{
-          title: 'Indkøbsliste',
-          tabBarLabel: 'Indkøbsliste',
+          title: "Indkøbsliste",
+          tabBarLabel: "Indkøbsliste",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="cart" color={color} size={size} />
+          ),
         }}
       />
       <Tabs.Screen
         name="Profile"
         options={{
-          title: 'Profil',
-          tabBarLabel: 'Profil',
+          title: "Profil",
+          tabBarLabel: "Profil",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
         }}
       />
     </Tabs>
